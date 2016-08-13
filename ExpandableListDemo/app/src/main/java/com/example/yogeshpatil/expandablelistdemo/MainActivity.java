f388a7c9d0b121d7d047ac3b999873d62659eb4a
yogeshdp <pyogesh092@gmail.com>
2016-08-13 11:24:46 +0530

    intermidiate commit

---
+++ ExpandableListDemo/app/src/main/java/com/example/yogeshpatil/expandablelistdemo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.yogeshpatil.expandablelistdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        final ArrayList<String> myBest = new ArrayList<String>();
+
+        myBest.add("Yogesh");
+        myBest.add("Asha");
+        myBest.add("Aarya");
+        myBest.add("Ashok");
+        myBest.add("Abhijit");
+        myBest.add("Dipak");
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myBest);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Log.i("Person Tapped", myBest.get(i));
+                Toast.makeText(getApplicationContext(), myBest.get(i).toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
